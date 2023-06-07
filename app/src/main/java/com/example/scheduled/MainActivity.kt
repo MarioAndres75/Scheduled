446f1d1b4ac949dd11ab3933e6225a37c52e83e6
MarioAndres75 <102887327+MarioAndres75@users.noreply.github.com>
2023-06-07 20:59:17 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/scheduled/MainActivity.kt
+++ app/src/main/java/com/example/scheduled/MainActivity.kt
@@ -14,62 +14,24 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {  lateinit var botonInicio: Button
-//autenticacion
-/*    private var signInLauncher = registerForActivityResult(
-        FirebaseAuthUIActivityResultContract(),
-    ) { res ->
-        this.onSignInResult(res)
-    } */
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
-
-
-        var actionCodeSettings = ActionCodeSettings.newBuilder()
-        .setAndroidPackageName( // yourPackageName=
-            "...", // installIfNotAvailable=
-            true, // minimumVersion=
-            null,
-        )
-        .setHandleCodeInApp(true) // This must be set to true
-        .setUrl("https://google.com") // This URL needs to be whitelisted
-        .build()
-
-       // autenticacion
-    /*   var providers = listOf(
-        AuthUI.IdpConfig.EmailBuilder()
-            .enableEmailLinkSignIn()
-            .setActionCodeSettings(actionCodeSettings)
-            .build(),
-    )
-    var signInIntent = AuthUI.getInstance()
-        .createSignInIntentBuilder()
-        .setAvailableProviders(providers)
-        .build()
-    signInLauncher.launch(signInIntent)
-
-
-
-
-
-        if (AuthUI.canHandleIntent(intent)) {
-            var extras = intent.extras ?: return
-            val link = extras.getString("email_link_sign_in")
-            if (link != null) {
-                var signInIntent = AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-                    .setEmailLink(link)
-                    .setAvailableProviders(providers)
-                    .build()
-                signInLauncher.launch(signInIntent)
-            }
-        }
-*/
-
         setContentView(R.layout.activity_main)
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
         botonInicio=findViewById(R.id.BotonInicio)
+
+        lateinit var auth: FirebaseAuth
+// ...
+// Initialize Firebase Auth
+        auth = Firebase.auth
+
+
+
+
+
 
         botonInicio.setOnClickListener{
             val lanzar = Intent(this,pantalla2::class.java) //home
@@ -78,21 +40,7 @@ class MainActivity : AppCompatActivity() {  lateinit var botonInicio: Button
 
 
     }
-    //autenticacion funcion
-    /* private fun onSignInResult(result:FirebaseAuthUIAuthenticationResult) {
-        var response = result.idpResponse
-        if (result.resultCode == RESULT_OK) {
-            // Successfully signed in
-            var user = FirebaseAuth.getInstance().currentUser
-            // ...
-        } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
-        }
-    }
-*/
+
 
 
 }
