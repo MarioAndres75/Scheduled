797759778ab404601aa080eb670758f7649a595f
MarioAndres75 <102887327+MarioAndres75@users.noreply.github.com>
2023-06-07 21:04:28 -0300

    Update pantalla2.kt

--- app/src/main/java/com/example/scheduled/pantalla2.kt
+++ app/src/main/java/com/example/scheduled/pantalla2.kt
@@ -2,6 +2,7 @@ package com.example.scheduled
 
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.nfc.Tag
 import com.google.firebase.analytics.FirebaseAnalytics
 import android.os.Bundle
 import android.util.Log
@@ -19,15 +20,18 @@ import com.google.firebase.database.ktx.snapshots
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import java.util.*
-
-var numeroDeEvento=0
+var ordenEvento=0
+var numeroDeEvento=1
 var tipoEvento = "Tipo No Identificado "
 var fecha = "01/01/2023"
 var detalle="sin detalle"
 var ArrayDeEventosNew: MutableList<EventoNew> = mutableListOf()
 var hora ="00"
-lateinit var database: DatabaseReference //realtime database
-var db = FirebaseFirestore.getInstance()
+var contador =0
+var cantidadDeEventos=0
+
+
+var db = FirebaseFirestore.getInstance() //firestore
 class pantalla2 : AppCompatActivity() {
 
     val c = Calendar.getInstance()
@@ -36,8 +40,8 @@ class pantalla2 : AppCompatActivity() {
     val ordenActual = (month*100)+day
     lateinit var nuevoEvento :Button
     lateinit var listaDeEventos:TextView
+    lateinit var planificacion:Button
     var otroEvento =0
-    var enLaBase="nada"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,66 +49,65 @@ class pantalla2 : AppCompatActivity() {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
         nuevoEvento=findViewById(R.id.NuevoEvento)
         listaDeEventos=findViewById(R.id.ListaDeEventos)
-
-       // enLaBase= "resultado ${database.child("Eventos").get().toString()}"
-
-        listaDeEventos.text=enLaBase // "${database.child("Eventos").get().toString()}"
-
-
-       FirebaseDatabase.getInstance().reference // realtime database
-        database = Firebase.database.reference
-
-         val listenerTablaPartidas: ChildEventListener = object : ChildEventListener {
-            override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
-
-            }
-            override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {
-
-            }
-            override fun onChildRemoved(dataSnapshot: DataSnapshot) {
-
-            }
-            override fun onChildMoved(dataSnapshot: DataSnapshot, s: String?) {
-
-            }
-            override fun onCancelled(databaseError: DatabaseError) {
-
-            }
-        }
-        database= FirebaseDatabase.getInstance().getReference("pri_dato")
-
-
-        ArrayDeEventosNew.sortBy { it.orden }//ordena la ajenda
-        ArrayDeEventosNew.forEach{
-            if (ordenActual<= it.orden){
-                listaDeEventos.text="${listaDeEventos.text} \n " +
-                        "${it.detalle.toString()}"
-            }else otroEvento =1
-        }
-        if(otroEvento==1){
-            otroEvento=0
-            listaDeEventos.text= "${listaDeEventos.text} \n " +
-                    "*************************** \n"+
-                    "2024 -> "
-        }
-        ArrayDeEventosNew.forEach{
-            if (ordenActual> it.orden){
-
-                listaDeEventos.text= "${listaDeEventos.text} \n " +
-                        "${it.detalle.toString()} \n"
-
-
-            }
-        }
+        planificacion=findViewById(R.id.planificacion)
+       leerFirestore()
+       ordenarImprimir()
 
         nuevoEvento.setOnClickListener{
 
             val lanzar = Intent(this,Pantalla3::class.java) //home
             startActivity(lanzar)
             numeroDeEvento++
+       }
+       planificacion.setOnClickListener {
+           leerFirestore()
+           ordenarImprimir()
+       }
+    }
+fun ordenarImprimir(){
+    ArrayDeEventosNew.sortBy { it.orden }//ordena la ajenda
+    ArrayDeEventosNew.forEach{
+        if (ordenActual<= it.orden){
+            listaDeEventos.text="${listaDeEventos.text} \n " +
+                    "${it.detalle.toString()}"
+        }else otroEvento =1
+    }
+    if(otroEvento==1){
+        otroEvento=0
+        listaDeEventos.text= "${listaDeEventos.text} \n " +
+                "*************************** \n"+
+                "2024 -> "
+    }
+    ArrayDeEventosNew.forEach{
+        if (ordenActual> it.orden){
+
+            listaDeEventos.text= "${listaDeEventos.text} \n " +
+                    "${it.detalle.toString()} \n"
 
 
         }
     }
+}
+fun leerFirestore(){
+    if (contador ==0) {
+        contador=1
+        for (i in 1..365) {
 
+            val docRef = db.collection("Eventos").document(i.toString())
+                .get()
+                .addOnSuccessListener {
+                    if (it.get("detalle") != null) {
+
+                       var NuevoEvento = (it.get("detalle") as String?).toString()
+                     var orden =  (it.get("orden") as String?).toString()
+                       val eventoNew = EventoNew(NuevoEvento,orden.toInt())
+                       cantidadDeEventos++
+                       ArrayDeEventosNew.add(eventoNew)
+                    }
+
+                }
+        }
+
+    }
+}
 }
